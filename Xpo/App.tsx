e40cdc7c06433fc44cf58c93bca3712dbad8409d
Talon Carballeira <taloncarb@gmail.com>
2024-02-02 16:28:16 -0700

    Accessing a database

--- Xpo/App.tsx
+++ Xpo/App.tsx
@@ -1,19 +1,67 @@
+
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Pressable } from 'react-native';
-import React from "react"
+import React, { useEffect } from "react"
+
+// make a new type
+type ToDoData = {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean
+}
+
+
+
 
 export default function App() {
-    const [count, setCount] = React.useState<number>(0)
-    const [toggle, setToggle] = React.useState<boolean>(false)
+    const [count, setCount] = React.useState<number>(0) 
+    const [toggle, setToggle] = React.useState<boolean>(false) 
+    const [toDoData, setToDoData] = React.useState<ToDoData>({userId: 0, id: 0, title: "", completed: false} )
+    const a = (x: number) => {console.log(x)}  
+    a(1)
+
     function onPress(){
         setCount(count+1)
     }
     function showText(){
         setToggle(!toggle)
     }
-  return (
+
+    // triggers a change. 
+    useEffect(() => { 
+      const options : RequestInit= { 
+        method: "GET" 
+      }
+      fetch(`https://jsonplaceholder.typicode.com/todos/${count}`,options).then( async(response) => {
+          const _json = await response.json()  // wait for everything to come in and then load it
+          const newData:ToDoData = {userId: 0, id: 0, title: "", completed: false} 
+
+          if (_json.userId && _json.title && typeof(_json.completed) === 'boolean' && _json.id) {
+            newData.userId = _json.userId
+            newData.id = _json.id
+            newData.title = _json.title
+            newData.completed = _json.completed
+            setToDoData(newData)
+          } 
+          else{
+            newData.title = "Test"
+            setToDoData(newData)
+          }
+          
+
+
+        }
+      )   
+    }, [count] )  
+
+  
+    return (
     <View style={styles.container}>
       {toggle? <Text>Open up App.tsx to start working on your!</Text>: null}
+      <Text style = {{
+        color: 'red'
+      }}> {toDoData.title} </Text>
 
       <Pressable onPress = {onPress} style = {styles.button}>
         <Text>{"Number " + count}</Text>
--- Xpo/ReadMe.md
+++ Xpo/ReadMe.md
@@ -5,3 +5,14 @@ Start expo:
 npx expo start
 
 Scan QR code
+
+
+Saving (committing and pushing to gitHub): 
+Go to source control on left side window. 
+Push the plus mark on docs with changes to be saved. 
+Commit changes with a message description
+Press Sync changes. 
+
+
+Work on making it so that the count only incraments when you swipe on the button. 
+Make it so that when it reaches 200 it resets and goes back to 1. 
